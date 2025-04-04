26ea5d58fff41fb84123d244129d664bed034d1a
khushi02raj <khushi02raj@gmail.com>
2025-04-04 16:41:02 +0530

    port

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import HotelRouter from './routes/hotel.js';
 import BookingRouter from './routes/booking.js';
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
